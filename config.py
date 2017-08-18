555e0ec226296c58edc3bcb33be25b8456af0c5a
Patrick Python Book <pwalukagga@gmail.com>
2017-08-18 08:59:14 +0300

    updated config file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\xde\x0e@Q\xd4\xc9\xf8n\x14\x91)\xfa\xd6\x14\xf5\xe8It\xfa\x08\xb7\xf0\x1f\xe9'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Development config
 class DevelopmentConfig(BaseConfig):
